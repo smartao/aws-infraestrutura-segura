12d4d841f01551886d27079b94aa363a6dc3e243
smartao <smartao@gmail.com>
2026-02-18 18:54:59 -0300

    finalizado modulo de network

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -33,3 +33,72 @@ resource "aws_subnet" "private" {
   }
 }
 # Reference: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+# Reference: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/internet_gateway
+
+resource "aws_eip" "nat" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.main]
+
+  tags = {
+    Name = "nat-eip"
+  }
+}
+# Reference: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id # Place NAT Gateway in the first public subnet
+
+  tags = {
+    Name = "main-nat-gateway"
+  }
+}
+# Reference: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/nat_gateway
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+# Reference: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name = "private-rt"
+  }
+}
+# Reference: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+# Reference: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+# Reference: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
